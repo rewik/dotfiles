be1b9d6b153ac47edf702213e3e62da3c921268b
Karol Szumski <karol.szumski@plum.pl>
2023-03-30 09:04:39 +0200

    (+) Added the :HelpLocal command to help me remember some of the     commands added to nvim by plugins

--- nvim/lua/cfg-plugins.lua
+++ nvim/lua/cfg-plugins.lua
@@ -11,6 +11,48 @@ end
 
 local packer_bootstrap = ensure_packer()
 
+vim.api.nvim_create_user_command(
+    'HelpLocal',
+    function(opts)
+        local buf = vim.api.nvim_create_buf(false, true)
+        vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
+        local width = vim.api.nvim_get_option("columns")
+        local height = vim.api.nvim_get_option("lines")
+        local win_height = math.ceil(height * 0.8 - 4)
+        local win_width = math.ceil(width * 0.8)
+
+        local win_x = math.ceil((width - win_width) / 2)
+        local win_y = math.ceil((height - win_height) / 2)
+
+        local opts = {
+            style = "minimal",
+            relative = "editor",
+            width = win_width,
+            height = win_height,
+            row = win_y,
+            col = win_x,
+            border = "rounded",
+        }
+
+        local win = vim.api.nvim_open_win(buf, true, opts)
+        vim.api.nvim_win_set_option(win, "cursorline", true)
+
+        vim.api.nvim_buf_set_lines(buf,0,0,false,
+            {"Helpful commands:",
+             ":PackerStatus",
+             "  - check status of packer (packages)",
+             "",
+             ":COQnow",
+             "  - enable code completion",
+             "",
+             "<l>b / <l>r / <l>R",
+             "  - list buffers / search buffers / search files?"
+            }
+        )
+    end,
+    { nargs = 0 }
+)
+
 return require('packer').startup(function(use)
     -- packer
     use {'wbthomason/packer.nvim'}
